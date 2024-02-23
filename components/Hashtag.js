9268da2f344e9e1bca08f2a2b34b42eadbc39630
Your name <Your email>
2024-02-23 15:56:43 +0100

    vercel

--- components/Hashtag.js
+++ components/Hashtag.js
@@ -30,7 +30,7 @@ function Hashtag() {
 
     setQuery('#' + hashtag);
 
-    fetch(`http://localhost:3000/tweets/hashtag/${user.token}/${hashtag}`)
+    fetch(`https://tweetapp-backend.vercel.app/tweets/hashtag/${user.token}/${hashtag}`)
       .then(response => response.json())
       .then(data => {
         data.result && dispatch(loadTweets(data.tweets));
